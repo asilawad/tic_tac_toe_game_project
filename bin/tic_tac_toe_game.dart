10595783c6033280e553cfd41beca7ba5b940a4e
Asil Awad <aseelawad8@hotmail.com>
2025-05-17 16:43:44 +0300

    first commit

---
+++ bin/tic_tac_toe_game.dart
@@ -0,0 +1,5 @@
+import 'package:tic_tac_toe_game/tic_tac_toe_game.dart' as tic_tac_toe_game;
+
+void main(List<String> arguments) {
+  print('Hello world: ${tic_tac_toe_game.calculate()}!');
+}
---
+++ test/tic_tac_toe_game_test.dart
@@ -0,0 +1,8 @@
+import 'package:tic_tac_toe_game/tic_tac_toe_game.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
