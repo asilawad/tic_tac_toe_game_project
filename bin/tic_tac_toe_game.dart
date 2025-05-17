a6121df55ba61ada4dcfb6d6f6c734684ac2fc6b
Asil Awad <aseelawad8@hotmail.com>
2025-05-17 16:56:30 +0300

    add chooseMarkers functionality

--- bin/tic_tac_toe_game.dart
+++ bin/tic_tac_toe_game.dart
@@ -3,12 +3,16 @@ import 'dart:io';
 class TicTacToe {
   // 1. Create a 3x3 board consisting of 9 cells
   List<String> board = List.generate(9, (index) => (index + 1).toString());
+  String currentPlayer = 'X'; // Current player
+  String playerX = 'X';
+  String playerO = 'O';
   bool isVsAI = false;
 
   // Start the game
   void startGame() {
     print("Welcome to Tic-Tac-Toe!");
     chooseMode();
+    chooseMarkers();
   }
 
   void printBoard() {
@@ -24,6 +28,19 @@ class TicTacToe {
     stdout.write("Do you want to play vs AI? (y/n): ");
     String? input = stdin.readLineSync();
     isVsAI = input?.toLowerCase() == 'y';
+  }
+
+  void chooseMarkers() {
+    stdout.write("Player 1, choose your marker (X or O): ");
+    String? marker = stdin.readLineSync();
+    if (marker != null &&
+        (marker.toUpperCase() == 'X' || marker.toUpperCase() == 'O')) {
+      playerX = marker.toUpperCase();
+      playerO = (playerX == 'X') ? 'O' : 'X';
+      currentPlayer = playerX;
+    } else {
+      print("Invalid input, defaulting to X vs O.");
+    }
   }
 }
 
